53dd11ab583a453f5112cac3dc46b34f2b92c627
vaidyasaivarun22 <vaisyasaivarun22@gmail.com>
2022-05-13 22:40:39 +0530

    added multiple background images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // title = 'todolistApp';
   numbers:string[]=[];
+  count:number=0;
   senditem(item: any,item2: any)
   {
     if(item != "" && item2 != "")
     {
       this.numbers.push(item+" - "+item2);
+      this.count++;
     }
   }
   deletecontact(index: any)
   {
     this.numbers.splice(index,1);
+    this.count--;
   }
 }
